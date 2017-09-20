c2d86d4411b9c50d7fdb3a84b5c563f41ff8fb87
viharkodakandla <viharkodakandla@gmail.com>
2017-09-19 21:30:09 -0400

    Assignment1

--- src/Guitar.java
+++ src/Guitar.java
@@ -1,43 +1,101 @@
+/**
+ * Guitar.java
+ * 09/10/17
+ */
+package test;
+
+/**
+ * Class: Object Oriented Analysis and Design Assignment 1 Student: vihar
+ * Kodakandla ID: @01392814
+ */
+
 public class Guitar {
 
-  private String serialNumber, builder, model, type, backWood, topWood;
-  private double price;
+	private String serialNumber, builder, model, type, backWood, topWood;
+	private double price;
 
-  public Guitar(String serialNumber, double price,
-                String builder, String model, String type,
-                String backWood, String topWood) {
-    this.serialNumber = serialNumber;
-    this.price = price;
-    this.builder = builder;
-    this.model = model;
-    this.type = type;
-    this.backWood = backWood;
-    this.topWood = topWood;
-  }
+	/**
+	 * 
+	 * @param serialNumber 
+	 * @param price
+	 * @param builder
+	 * @param model
+	 * @param type
+	 * @param backWood
+	 * @param topWood
+	 */
+	public Guitar(String serialNumber, double price, String builder, String model, String type, String backWood,
+			String topWood) {
+		this.serialNumber = serialNumber;
+		this.price = price;
+		this.builder = builder;
+		this.model = model;
+		this.type = type;
+		this.backWood = backWood;
+		this.topWood = topWood;
+	}
 
-  public String getSerialNumber() {
-    return serialNumber;
-  }
+	/**
+	 * 
+	 * @return serialNumber
+	 */
+	public String getSerialNumber() {
+		return serialNumber;
+	}
 
-  public double getPrice() {
-    return price;
-  }
-  public void setPrice(float newPrice) {
-    this.price = newPrice;
-  }
-  public String getBuilder() {
-    return builder;
-  }
-  public String getModel() {
-    return model;
-  }
-  public String getType() {
-    return type;
-  }
-  public String getBackWood() {
-    return backWood;
-  }
-  public String getTopWood() {
-    return topWood;
-  }
+	/**
+	 * 
+	 * @return price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/**
+	 * 
+	 * @param newPrice
+	 */
+	public void setPrice(float newPrice) {
+		this.price = newPrice;
+	}
+
+	/**
+	 * 
+	 * @return builder
+	 */
+	public String getBuilder() {
+		return builder;
+	}
+
+	/**
+	 * 
+	 * @return model
+	 */
+	public String getModel() {
+		return model;
+	}
+
+	/**
+	 * 
+	 * @return type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * 
+	 * @return backwood
+	 */
+	public String getBackWood() {
+		return backWood;
+	}
+
+	/**
+	 * 
+	 * @return topwood
+	 */
+	public String getTopWood() {
+		return topWood;
+	}
 }
